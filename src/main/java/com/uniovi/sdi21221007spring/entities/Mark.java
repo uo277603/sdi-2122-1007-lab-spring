78280a067384e6f190e72bf5e0f7e1bd702b0a63
Daniel Ferreira Gomez <danix@192.168.1.226>
2022-02-07 19:07:54 +0100

    SDI-2122-1007-2.4-Modelo, acceso a datos simple.

--- src/main/java/com/uniovi/sdi21221007spring/entities/Mark.java
+++ src/main/java/com/uniovi/sdi21221007spring/entities/Mark.java
@@ -1,7 +1,14 @@
 package com.uniovi.sdi21221007spring.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Mark {
 
+    @Id
+    @GeneratedValue
     private Long id;
     private String description;
     private Double score;
---
+++ src/main/java/com/uniovi/sdi21221007spring/repositories/MarksRepository.java
@@ -0,0 +1,7 @@
+package com.uniovi.sdi21221007spring.repositories;
+
+import com.uniovi.sdi21221007spring.entities.Mark;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MarksRepository extends CrudRepository<Mark, Long> {
+}
