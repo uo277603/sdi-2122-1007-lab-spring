bc9570e1e7c61abd3e3fc8b01e07d7a8b2216054
uo277603 <uo277603@uniovi.es>
2022-02-14 12:18:31 +0100

    SDI-2122-1007-3.1-Thymeleaf, fragmentos.

--- src/main/java/com/uniovi/sdi21221007spring/controllers/MarksController.java
+++ src/main/java/com/uniovi/sdi21221007spring/controllers/MarksController.java
@@ -56,5 +56,11 @@ public class MarksController {
         return "redirect:/mark/details/"+id;
     }
 
+    @RequestMapping("/mark/list/update")
+    public String updateList(Model model){
+        model.addAttribute("markList", marksService.getMarks());
+        return "mark/list::tableMarks";
+    }
+
 
 }
