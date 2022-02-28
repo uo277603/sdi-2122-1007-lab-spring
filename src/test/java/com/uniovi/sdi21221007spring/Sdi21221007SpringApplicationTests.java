5d3fda7293d10037054bc090ed3acdb722de4e2b
Daniel Ferreira Gomez <danix@192.168.1.226>
2022-02-28 17:03:13 +0100

    SDI-2122-1007-5.4-Casos Uso VistaHome.

--- src/test/java/com/uniovi/sdi21221007spring/Sdi21221007SpringApplicationTests.java
+++ src/test/java/com/uniovi/sdi21221007spring/Sdi21221007SpringApplicationTests.java
@@ -1,9 +1,14 @@
 package com.uniovi.sdi21221007spring;
 
+import com.uniovi.sdi21221007spring.pageobjects.PO_HomeView;
+import com.uniovi.sdi21221007spring.pageobjects.PO_Properties;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -15,7 +20,7 @@ class Sdi21221007SpringApplicationTests {
 
     static String PathFirefox = "C:\\Program Files\\Mozilla Firefox\\firefox.exe";
     //static String Geckodriver = "C:\\Path\\geckodriver-v0.30.0-win64.exe";
-    static String Geckodriver = "C:\\Users\\uo277603\\Documents\\SDI\\Sesion5\\PL-SDI-Sesión5-material\\geckodriver-v0.30.0-win64.exe";
+    static String Geckodriver = "D:\\ClaseTercero\\SegundoCuatri\\SDI\\Practicas\\PL-SDI-Sesión5-material\\geckodriver-v0.30.0-win64.exe";
     //static String PathFirefox = "/Applications/Firefox.app/Contents/MacOS/firefox-bin";
     //static String Geckodriver = "/Users/USUARIO/selenium/geckodriver-v0.30.0-macos";
     //Común a Windows y a MACOSX
@@ -87,6 +92,43 @@ class Sdi21221007SpringApplicationTests {
     @Test
     @Order(10)
     void PRO010() {}
+
+    @Test
+    @Order(1)
+    void PR01A() {
+        PO_HomeView.checkWelcomeToPage(driver, PO_Properties.getSPANISH());
+    }
+
+    @Test
+    @Order(2)
+    void PR01B() {
+        List<WebElement> welcomeMessageElement = PO_HomeView.getWelcomeMessageText(driver,
+                PO_Properties.getSPANISH());
+        Assertions.assertEquals(welcomeMessageElement.get(0).getText(),
+                PO_HomeView.getP().getString("welcome.message", PO_Properties.getSPANISH()));
+    }
+
+    //PR02. Opción de navegación. Pinchar en el enlace Registro en la página home
+    @Test
+    @Order(3)
+    public void PR02() {
+        PO_HomeView.clickOption(driver, "signup", "class", "btn btn-primary");
+    }
+    //PR03. Opción de navegación. Pinchar en el enlace Identifícate en la página home
+    @Test
+    @Order(4)
+    public void PR03() {
+        PO_HomeView.clickOption(driver, "login", "class", "btn btn-primary");
+    }
+
+    //PR04. Opción de navegación. Cambio de idioma de Español a Inglés y vuelta a Español
+    @Test
+    @Order(5)
+    public void PR04() {
+        PO_HomeView.checkChangeLanguage(driver, "btnSpanish", "btnEnglish",
+                PO_Properties.getSPANISH(), PO_Properties.getENGLISH());
+    }
+
 
 
 
