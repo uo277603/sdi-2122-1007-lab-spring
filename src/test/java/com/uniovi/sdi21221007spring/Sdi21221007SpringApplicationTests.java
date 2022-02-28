6dc028f321c8360a8eda3569e068ce58ef0a760b
uo277603 <uo277603@uniovi.es>
2022-02-28 12:26:54 +0100

    SDI-2122-1007-5.1-Clase principal.

--- src/test/java/com/uniovi/sdi21221007spring/Sdi21221007SpringApplicationTests.java
+++ src/test/java/com/uniovi/sdi21221007spring/Sdi21221007SpringApplicationTests.java
@@ -1,13 +1,93 @@
 package com.uniovi.sdi21221007spring;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class Sdi21221007SpringApplicationTests {
 
     @Test
     void contextLoads() {
     }
+
+    static String PathFirefox = "C:\\Program Files\\Mozilla Firefox\\firefox.exe";
+    //static String Geckodriver = "C:\\Path\\geckodriver-v0.30.0-win64.exe";
+    static String Geckodriver = "C:\\Users\\uo277603\\Documents\\SDI\\Sesion5\\PL-SDI-Sesión5-material\\geckodriver-v0.30.0-win64.exe";
+    //static String PathFirefox = "/Applications/Firefox.app/Contents/MacOS/firefox-bin";
+    //static String Geckodriver = "/Users/USUARIO/selenium/geckodriver-v0.30.0-macos";
+    //Común a Windows y a MACOSX
+    static WebDriver driver = getDriver(PathFirefox, Geckodriver);
+    static String URL = "http://localhost:8090";
+
+    public static WebDriver getDriver(String PathFirefox, String Geckodriver) {
+        System.setProperty("webdriver.firefox.bin", PathFirefox);
+        System.setProperty("webdriver.gecko.driver", Geckodriver);
+        driver = new FirefoxDriver();
+        return driver;
+    }
+
+    @BeforeEach
+    public void setUp(){
+        driver.navigate().to(URL);
+    }
+    //Después de cada prueba se borran las cookies del navegador
+    @AfterEach
+    public void tearDown(){
+        driver.manage().deleteAllCookies();
+    }
+    //Antes de la primera prueba
+    @BeforeAll
+    static public void begin() {}
+    //Al finalizar la última prueba
+    @AfterAll
+    static public void end() {
+        //Cerramos el navegador al finalizar las pruebas
+        driver.quit();
+    }
+
+    @Test
+    @Order(1)
+    void PRO01() {}
+
+    @Test
+    @Order(2)
+    void PRO02() {}
+
+    @Test
+    @Order(3)
+    void PRO03() {}
+
+    @Test
+    @Order(4)
+    void PRO04() {}
+
+    @Test
+    @Order(5)
+    void PRO05() {}
+
+    @Test
+    @Order(6)
+    void PRO06() {}
+
+    @Test
+    @Order(7)
+    void PRO07() {}
+
+    @Test
+    @Order(8)
+    void PRO08() {}
+
+    @Test
+    @Order(9)
+    void PRO00() {}
+
+    @Test
+    @Order(10)
+    void PRO010() {}
+
+
 
 }
